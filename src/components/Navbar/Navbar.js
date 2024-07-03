d6acd3b4649df71c42b2269cf3cfaca4ed6801cb
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-07-03 22:10:47 +0900

    編集

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import "./Navbar.css"
 
-// const Navbar = () => {
-//     return (
-//         <nav>ポケモン図鑑</nav>
-//     )
-// }
+const Navbar = () => {
+    return (
+        <nav>ポケモン図鑑</nav>
+    )
+}
 
 export default Navbar
